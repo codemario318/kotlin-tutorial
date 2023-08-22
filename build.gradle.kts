4a2fd8fc3838d990f438eb87ce806200dc7fb620
mario <codemario318@gmail.com>
2023-08-22 20:51:02 +0900

    Config properties 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 	kotlin("plugin.spring") version "1.8.22"
 	kotlin("plugin.jpa") version "1.8.22"
 	kotlin("plugin.allopen") version "1.8.0"
+	kotlin("kapt") version "1.8.0"
 }
 
 group = "com.mario"
@@ -35,6 +36,7 @@ dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter-api")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 	testImplementation("com.ninja-squad:springmockk:4.0.0")
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/com/mario/kotlintutorial/KotlinTutorialApplication.kt
+++ src/main/kotlin/com/mario/kotlintutorial/KotlinTutorialApplication.kt
@@ -1,9 +1,11 @@
 package com.mario.kotlintutorial
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class KotlinTutorialApplication
 
 fun main(args: Array<String>) {
